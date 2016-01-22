53d182e3431753abab1bb7d33db3e2a29573384a
Antonio Ruberto <aruberto@users.noreply.github.com>
2016-01-22 15:29:52 -0500

    switch to stages to match power point

---
+++ src/stage-1.2/app.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import parties from './parties';
+import VoteForParty from './components/vote-for-party';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {parties.map((party) => <VoteForParty party={party}/>)}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('content'));
